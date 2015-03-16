a6b8bac4cf76911d4b9aecdbc7704b030da66cad
Huitre <gohin.j@gmail.com>
2015-03-16 22:28:14 +0100

    Minimap is now functional Need to make this minimap a separate React d3 component.

--- src/app/components/charts/linechart.jsx
+++ src/app/components/charts/linechart.jsx
@@ -4,50 +4,60 @@ var React = require('react'),
 
 // d3Chart.js
 
-var d3Chart = {};
-
-d3Chart.create = function(el, props, state) {
-  console.log('d3.create');
+var d3Chart = function (props) {
+  //console.log('d3Chart constructor');
   var margin = {top: 30, right: 20, bottom: 30, left: 50},
       width = props.width,
       context = {h: 90, width: width},
-      height = props.height,
-      xAxis, yAxis,
-      svg = d3.select(el), focus, context;
+      height = props.height;
 
-  this.dimensions = {w: width, h: height - context.h - margin.top - margin.bottom, margin: margin};
-  this.context = context;
-  
+  this.dimensions = {
+    w: width - margin.left - margin.right, 
+    h: height - context.h - margin.bottom,
+    ow: width,
+    oh: height,
+    margin: margin, 
+    context: context
+  };
+};
+
+d3Chart.prototype.create = function(el, state) {
+  //console.log('d3.create');
+
+  var xAxis, yAxis,
+      svg = d3.select(el), focus, context,
+      margin = this.dimensions.margin;
+
   svg = svg.append('svg')
         .attr('class', 'd3')
-        .attr('width', width)
-        .attr('height', height)
+        .attr('width', this.dimensions.ow)
+        .attr('height', this.dimensions.oh)
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   focus = svg.append("g")
     .attr("class", "focus")
-    .attr('width', width)
-    .attr('height', height)
-    .attr('clip-path', 'url(#clip)')
+    .attr('width', this.dimensions.ow)
+    .attr('height', this.dimensions.oh)
 
   context = svg.append("g")
     .attr("class", "context")
-    .attr('height', context.h)
-    .attr("transform", "translate(0," + (margin.top + context.h) + ")")
+    .attr('height', this.dimensions.context.h)
+    .attr("transform", "translate(0," + (margin.top + this.dimensions.context.h) + ")")
   
   svg.append("defs").append("clipPath")
       .attr("id", "clip")
     .append("rect")
-      .attr("width", width)
-      .attr("height", height);
+      .attr("width", this.dimensions.w - margin.left - margin.right)
+      .attr("height", this.dimensions.h)
+    .attr("transform", "translate(" + (margin.left + 1) + ",0)");
 
   var scales = this.scales(el, state.domain);
 
   this.update(el, state);
 }
 
-d3Chart.update = function(el, state) {
-  console.log('d3.update');
+d3Chart.prototype.update = function(el, state) {
+  //console.log('d3.update');
 
   // Re-compute the scales, and render the data points
   var scales = this.scales(el, state.domain);
@@ -55,32 +65,35 @@ d3Chart.update = function(el, state) {
 }
 
 
-d3Chart.destroy = function(el) {
-  // Any clean-up would go here
-  // in this example there is nothing to do
+d3Chart.prototype.destroy = function(el) {
+
 }
 
-d3Chart.scales = function (el, domain) {
-    console.log('d3.scale');
+d3Chart.prototype.scales = function (el, domain) {
+    //console.log('d3.scale', this.dimensions);
     if (!domain) {
       return null;
     }
   
     var width = this.dimensions.w,
         height = this.dimensions.h, 
-        x, y, y2;
+        x, x2, y, y2;
 
     x = d3.time.scale()
       .domain(domain.x)
       .rangeRound([this.dimensions.margin.left, width - this.dimensions.margin.right]);
-  
+
     y = d3.scale.linear()
       .domain(domain.y)
       .rangeRound([height - this.dimensions.margin.top -this.dimensions.margin.bottom, -this.dimensions.margin.bottom]);
 
+    x2 = d3.time.scale()
+      .domain(domain.x)
+      .rangeRound([this.dimensions.margin.left, width - this.dimensions.margin.right]);
+  
     y2 = d3.scale.linear()
       .domain(domain.y)
-      .rangeRound([this.context.h, 0]);
+      .rangeRound([this.dimensions.context.h, 0]);
 
     z = 1;
     
@@ -88,114 +101,127 @@ d3Chart.scales = function (el, domain) {
       x: x, 
       y: y, 
       context: {
-        x: x, 
+        x: x2, 
         y: y2
       }
     };
 }
 
-d3Chart.drawAxes = function (el, scales, data) {
+d3Chart.prototype.drawAxes = function (el, scales, data) {
 
 }
 
-d3Chart.drawPoints = function (el, scales, data) {
-  console.log('d3.drawPoints')
+d3Chart.prototype.drawPoints = function (el, scales, data) {
+  //console.log('d3.drawPoints', el, scales, data);
   var drawLines, // draw method for main area 
       drawContextLines, // draw method for minimap
       context, // our minimap
       focus, // main content
       xAxis, // generic x axis with time scale
       yAxis, // generic y axis with scale
-      svg;
+      xAxisContext, // generic axis for minimap
+      svg,
+      Xdomain = scales.x.domain();
 
   
   xAxis = d3.svg.axis()
             .scale(scales.x)
             .orient("bottom")
-  
+  xAxisContext = d3.svg.axis()
+            .scale(scales.context.x)
+            .orient("bottom")
+
+
   yAxis = d3.svg.axis()
             .scale(scales.y)
             .orient("left");
   
   drawLines = d3.svg.line()
                 .x(function (d) { 
-                  return scales.x(new Date(d.x).getTime()); 
+                  return scales.x(d.x); 
                 })
                 .y(function (d) { return scales.y(d.y); })
                 .interpolate("basis");
 
-  drawContextLines = d3.svg.line()
-                      .x(function (d) { 
-                        return scales.x(new Date(d.x).getTime()); 
-                      })
-                      .y(function (d) { return scales.context.y(d.y); })
-                      .interpolate("basis");
-
   svg = d3.select(el).selectAll('.focus');
   focus = d3.select(el).selectAll('.focus');
-  context = d3.select(el).selectAll('.context');
   
+  // we create x axxis and y axxis
   svg.append('g').attr('class', 'x axis').attr('transform', 'translate(0,' + (this.dimensions.h - this.dimensions.margin.bottom) + ')').call(xAxis);
   svg.append('g').attr('transform', 'translate(' + (this.dimensions.margin.left) + ',' + (this.dimensions.margin.top) + ')')
             .attr('class', 'axis').call(yAxis);
 
-  focus.append('path').attr('d', drawLines(data, scales))
+                
+  focus.append('path')
+          .datum(data)
+          .attr('class', 'chart')
+          .attr('d', drawLines)
+          .attr('transform', 'translate(0,' + (this.dimensions.margin.bottom) + ')')
           .style('stroke-width', 1)
           .style('fill', '#fff')
-          .style('stroke', '#3F51B5');
+          .style('stroke', '#3F51B5')
+          .attr('clip-path', 'url(#clip)')
+
+  // minimap goes here
+  context = d3.select(el).selectAll('.context');
+
+  drawContextLines = d3.svg.line()
+                      .x(function (d) { 
+                        return scales.context.x(d.x); 
+                      })
+                      .y(function (d) { return scales.context.y(d.y); })
+                      .interpolate("basis");
 
   context.append("g")
       .attr("class", "x axis")
       .attr("transform", 'translate(0,' + (this.dimensions.h - this.dimensions.margin.bottom) + ')')
-      .call(xAxis);
+      .call(xAxisContext);
 
   
   context.append('path')
-          .attr("transform", 'translate(0,' + (this.dimensions.h - this.context.h - this.dimensions.margin.bottom) + ')')
-          .attr('d', drawContextLines(data, scales))
+          .datum(data)
+          .attr("transform", 'translate(0,' + (this.dimensions.h - this.dimensions.context.h - this.dimensions.margin.bottom) + ')')
+          .attr('d', drawContextLines)
           .style('stroke-width', 1)
           .style('fill', '#fff')
           .style('stroke', '#3F51B5')
 
-  var self = this;
+  // minimap selection and events
   function brushed() {
-    //scales.x.domain(brush.empty() ? scales.x.domain() : brush.extent());
-    //d3.time.scale().range([0, self.dimensions.w]).domain(brush.empty() ? scales.x.domain() : brush.extent())
-    focus.select(".path").attr("d", drawLines(data, scales));
+    scales.x.domain(brush.empty() ? Xdomain : brush.extent());
+    focus.select("path.chart").attr("d", drawLines);
     focus.select(".x.axis").call(xAxis);
   }
-
   var brush = d3.svg.brush()
-    .x(scales.x)
+    .x(scales.context.x)
     .on("brush", brushed);
 
-  context.append("g")
+ context.append("g")
       .attr("class", "x brush")
-      .attr("transform", 'translate(0,' + (this.dimensions.h - this.context.h - this.dimensions.margin.bottom) + ')')
+      .attr("transform", 'translate(0,' + (this.dimensions.h - this.dimensions.context.h - this.dimensions.margin.bottom) + ')')
       .call(brush)
     .selectAll("rect")
       .attr("y", -6)
-      .attr("height", this.context.h + 7);
+      .attr("height", this.dimensions.context.h + 7);
+
+
 }
 
-var LineChart = React.createClass({displayName: 'LineChart',
+var Minimap = React.createClass({displayName: 'Minimap',
   propTypes: {
     data: React.PropTypes.array,
-    domain: React.PropTypes.object
+    domain: React.PropTypes.object,
+    chart: React.PropTypes.object
   },
 
   componentDidMount: function() {
     var el = this.getDOMNode();
-    
-    d3Chart.create(el, {
-      width: this.props.width,
-      height: this.props.height
-    }, this.getChartState());
   },
 
   componentDidUpdate: function() {
     var el = this.getDOMNode();
-    d3Chart.update(el, this.getChartState());
+    
+    /*this.d3Chart.update(el, this.getChartState());*/
   },
 
   getChartState: function() {
@@ -205,12 +231,59 @@ var LineChart = React.createClass({displayName: 'LineChart',
     };
   },
 
-  _getDomain: function (data) {
+  componentWillUnmount: function() {
+    var el = this.getDOMNode();
+  },
+
+  render: function() {
+    return (
+      <div className="Chart"></div>
+    );
+  }
+});
+
+var LineChart = React.createClass({displayName: 'LineChart',
+  propTypes: {
+    data: React.PropTypes.array,
+    domain: React.PropTypes.object    
+  },
+
+  componentDidMount: function() {
+   // console.log('LineChart.didmount')
+    this._id = this.generateId();
+    this.el = document.querySelector('#' + this._id);
+    //console.log(this.el, this._id, this.getDOMNode());
+    this.d3Chart = new d3Chart({
+        width: this.props.width,
+        height: this.props.height
+      });
+
+    this.d3Chart.create(this.getDOMNode(), this.getChartState());
+  },
+
+  componentDidUpdate: function() {
+    var el = this.getDOMNode(); 
+    
+    /*this.d3Chart.update(this.getDOMNode(), this.getChartState());*/
+  },
+
+  getChartState: function() {
+    return {
+      data: this.props.data,
+      domain: this.props.domain || this.getDomain(this.props.data)
+    };
+  },
+
+  generateId: function () {
+    return 'react-d3chart-' + (Math.ceil(Math.random() * 100))
+  },
+
+  getDomain: function (data) {
     var domain = { x : 0, y : 0}
 
     domain.x = [
-            d3.min(data, function (d) { return new Date(d.x).getTime() }),
-            d3.max(data, function (d) { return new Date(d.x).getTime() })
+            d3.min(data, function (d) { return new Date(d.x) }),
+            d3.max(data, function (d) { return new Date(d.x) })
         ];
     domain.y = [
           d3.min(data, function (d) { return d.y }), 
@@ -221,14 +294,15 @@ var LineChart = React.createClass({displayName: 'LineChart',
   },
 
   componentWillUnmount: function() {
-    var el = this.getDOMNode();
+    var el = this.el;
     
-    d3Chart.destroy(el);
+    this.d3Chart.destroy(el);
   },
 
   render: function() {
+    //console.log('LineChart.render')
     return (
-      <div className="Chart"></div>
+      <div className="Chart" id={this._id}></div>
     );
   }
 });
--- src/app/components/pages/stats.jsx
+++ src/app/components/pages/stats.jsx
@@ -8,167 +8,40 @@ var React = require('react'),
     Post = require('../postlist.jsx').Post,
     HappBar = require('../happbar.jsx');
 
-var StatsList = React.createClass({displayName: 'StatsList',
-
-  // Display a zoomable line chart with distance values
-  renderSpeed : function (data) {
-    if (data.speed) {
-      //this.props.data.speed.datas = data.speed.datas.slice(0, 40);
-      var speedStatsData = [{
-            name : "vitesse en " + data.speed.units,
-            values : null
-          }];
-      speedStatsData[0].values = data.speed.data.map(function (data) {
-        return {
-          x : new Date(data.time),
-          y : data.speed
-        }
-      });
-      
-      speedStats = (
-          <Post>
-            <LineChart
-              data={speedStatsData}
-              width={1400}
-              height={600}
-              legend={true}
-              xAxisTickInterval={{unit: 'minutes', interval: 1}}  title="Vitesse"/>
-          </Post>
-        );
-      return speedStats;
-    }
-    return null;
-  },
-  
-  // Display a zoomable line chart with distance values
-  renderDistance : function (data) {
-     if (data.distance) {
-      //this.props.data.distance.datas = this.props.data.distance.datas.slice(0, 300);
-      var distanceStatsData = [{
-            name : "distance en " + data.distance.units,
-            values : null
-          }];
-      distanceStatsData[0].values = data.distance.data.map(function (data) {
-        return {
-          x : new Date(data.createdAt),
-          y : data.distance * 1000
-        }
-      });
-      
-      distanceStats = (
-          <Post ref="post">
-            <LineChart
-              data={distanceStatsData[0].values}
-              width={1400}
-              height={700}
-              legend={true}
-              xAxisTickInterval={{unit: 'minutes', interval: 1}}  title="Distance"/>
-          </Post>
-        );
-      return distanceStats;
-    }
-    return null;
-  },
-
-  // Display a pie chart with percentage of activities
-  renderSummary : function () {
-
-  },
-
-  render: function () {    
-    var stats = [],
-        summary = null;
-
-    if (this.props.data.daily) {
-      stats.push(this.renderDistance(this.props.data.daily))
-      //stats.push(this.renderSpeed(this.props.data.daily))
-    }
-
-    if (this.props.data.weekly) {
-      //stats.push(this.renderDistance(this.props.data.weekly))
-      //stats.push(this.renderSpeed(this.props.data.weekly))
-    }
-
-    if (this.props.data.monthly) {
-      //stats.push(this.renderDistance(this.props.data.monthly))
-  //    stats.push(this.renderSpeed(this.props.data.monthly))
-    }
-    
-    return (
-      <section>
-        {summary}
-        {stats}
-      </section>
-    )
-  }
-})
 
 var Stats = React.createClass({displayName: 'Stats',
   getInitialState: function() {
     return {
-        daily : null,
-        weekly : null,
-        monthly : null,
-        summary : null
+        "daily" : null,
+        "weekly" : null,
+        "monthly" : null,
+        "summary" : null
     };
   },
 
   load : function () {
-    var req = H.ajax({
-          url : Config.api.url + '/me/stats',
-          credentials: true,
-          type: 'json',
-          success : function (res, req) {
-            this.setState({
-              daily : res.stats,
-              weekly : this.state.weekly,
-              monthly : this.state.monthly,
-              summary : this.state.summary
-            })
-          }.bind(this),
-          error : function (data, status,err) {
-            console.error(status, err.toString());
-            this.transitionTo('/');
-          }
-        })
-    req.send();
-    /*var req2 = H.ajax({
-          url : Config.api.url + '/me/stats/weekly',
-          credentials: true,
-          type: 'json',
-          success : function (res, req) {
-            console.log(res, req)
-            this.setState({
-              daily : this.state.daily,
-              weekly : res.stats,
-              monthly : this.state.monthly,
-              summary : this.state.summary
-            })
-          }.bind(this),
-          error : function (data, status,err) {
-            console.error(status, err.toString());
-            this.transitionTo('/');
-          }
-        })
-    req2.send();
-    /*var req3 = H.ajax({
-          url : Config.api.url + '/me/stats/monthly',
-          credentials: true,
-          type: 'json',
-          success : function (res, req) {
-            this.setState({
-              daily : this.state.daily,
-              weekly : this.state.weekly,
-              monthly : res.stats,
-              summary : this.state.summary
-            })
-          }.bind(this),
-          error : function (data, status,err) {
-            console.error(status, err.toString());
-            this.transitionTo('/');
-          }
-        })
-    req3.send();*/
+    var stats = ['daily', 'weekly', 'monthly'], self = this;
+
+    for (var i in stats) {
+      (function (type, self) {
+        var req = H.ajax({
+            url : Config.api.url + '/me/stats/' + (stats[i]),
+            credentials: true,
+            type: 'json',
+            success : function (res, req) {
+              var oldState = self.state;
+              
+              oldState[type] = res.stats;
+              self.setState(oldState);
+            },
+            error : function (data, status,err) {
+              console.error(status, err.toString());
+              this.transitionTo('/');
+            }
+          })
+        req.send();
+      }(stats[i], self));
+    }
   },
 
   componentDidMount: function() {
@@ -176,16 +49,37 @@ var Stats = React.createClass({displayName: 'Stats',
   },
 
   render: function() {
+    
+    var distanceStats = [], data;
+
+    for (var type in this.state) {
+      if (this.state[type] && this.state[type].hasOwnProperty('distance')) {
+        data = this.state[type].distance.data.map(function (data) {
+          return {
+            x : new Date(data.createdAt),
+            y : data.distance * 1000
+          }
+        });
+        distanceStats.push(
+          <Post ref="post">
+            <LineChart
+              data={data}
+              width={1400}
+              height={700}
+              legend={true}
+              xAxisTickInterval={{unit: 'minutes', interval: 1}}  title="Distance"/>
+          </Post>
+        )
+      }
+    }
+
     return (
       <section id="content">
         <HappBar
-        title="Hamsterace > me > stats" 
-        onClickHandler={this.onNewPostClick}/>
-        <StatsList 
-          data={this.state} 
-          onPostFormSubmit={this.onPostFormSubmit} 
-          action="/me/feed/post/" 
-          avatar="images/avatar-not-found.gif"/>
+        title="Hamsterace > me > stats" />
+        <section>
+        {distanceStats}
+        </section>
       </section>
     );
   }  
